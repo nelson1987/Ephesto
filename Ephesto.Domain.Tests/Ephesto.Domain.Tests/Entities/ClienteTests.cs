9fb349383046edcebcdf3d2c99a7f10ae880ea16
nelson1987 <nelson.ash@outlook.com>
2016-11-04 20:15:39 -0200

    Commit Inicial - Tests & Moq

---
+++ Ephesto.Domain.Tests/Ephesto.Domain.Tests/Entities/ClienteTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ephesto.Domain.Entities;
+
+namespace Ephesto.Domain.Tests.Entities
+{
+    [TestClass]
+    public class ClienteTests
+    {
+        [TestMethod]
+        public void CriarCliente()
+        {
+            Cliente cliente = new Cliente("Globo.com","globo_esporte");
+            Assert.AreEqual(cliente.Login, "globo_esporte");
+        }
+    }
+}
---
+++ Ephesto.Domain.Tests/Ephesto.Repository.Tests/Services/ClienteServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Ephesto.Domain;
+using Ephesto.Domain.Entities;
+using Ephesto.Service.Interfaces;
+using Ephesto.Service.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Ephesto.Service.Tests.Services
+{
+    [TestClass]
+    public class ClienteServiceTests
+    {
+        private ClienteService _target;
+        private Mock<IClienteRepository> _mock;
+
+        [TestMethod]
+        public void CriarCliente()
+        {
+            //Arrange
+            _mock = new Mock<IClienteRepository>();
+            _target = new ClienteService(_mock.Object);
+            
+            //Act
+            Cliente cliente = new Cliente("General Motors","general_motors");
+            _mock.Setup(x => x.BuscarPorId(It.IsAny<int>()))
+                .Returns(cliente);
+
+            //Assert
+            Cliente esperado = _target.CriarCliente(cliente);
+            Cliente atual = new Cliente(1, "General Motors", "general_motors");
+            Assert.AreEqual(esperado.Login,atual.Login);
+        }
+    }
+}
