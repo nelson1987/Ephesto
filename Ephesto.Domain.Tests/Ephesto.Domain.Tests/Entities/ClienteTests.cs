fe7633f843899c4b9f0337d34e3dfe1fe4de7fdc
nelson1987 <nelson.ash@outlook.com>
2016-11-05 09:09:12 -0200

    Inclusao do Namespace Ephesto.Domain.Tests

--- Ephesto.Domain.Tests/Ephesto.Domain.Tests/Entities/ClienteTests.cs
+++ Ephesto.Domain.Tests/Ephesto.Domain.Tests/Entities/ClienteTests.cs
@@ -1,6 +1,5 @@
-﻿using System;
+﻿using Ephesto.Domain.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Ephesto.Domain.Entities;
 
 namespace Ephesto.Domain.Tests.Entities
 {
@@ -10,7 +9,7 @@ namespace Ephesto.Domain.Tests.Entities
         [TestMethod]
         public void CriarCliente()
         {
-            Cliente cliente = new Cliente("Globo.com","globo_esporte");
+            var cliente = new Cliente("Globo.com", "globo_esporte");
             Assert.AreEqual(cliente.Login, "globo_esporte");
         }
     }
--- Ephesto.Domain.Tests/Ephesto.Domain.Tests/Services/ClienteServiceTests.cs
+++ Ephesto.Domain.Tests/Ephesto.Domain.Tests/Services/ClienteServiceTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Ephesto.Domain.Entities;
+﻿using Ephesto.Domain.Entities;
 using Ephesto.Domain.Interfaces.Repositories;
 using Ephesto.Domain.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,8 +9,8 @@ namespace Ephesto.Domain.Tests.Services
     [TestClass]
     public class ClienteServiceTests
     {
-        private ClienteService _target;
         private Mock<IClienteRepository> _mock;
+        private ClienteService _target;
 
         [TestMethod]
         public void CriarCliente()
@@ -21,13 +20,13 @@ namespace Ephesto.Domain.Tests.Services
             _target = new ClienteService(_mock.Object);
 
             //Act
-            Cliente cliente = new Cliente("General Motors", "general_motors");
+            var cliente = new Cliente("General Motors", "general_motors");
             _mock.Setup(x => x.BuscarPorId(It.IsAny<int>()))
                 .Returns(cliente);
 
             //Assert
-            Cliente esperado = _target.CriarCliente(cliente);
-            Cliente atual = new Cliente(1, "General Motors", "general_motors");
+            var esperado = _target.CriarCliente(cliente);
+            var atual = new Cliente(1, "General Motors", "general_motors");
             Assert.AreEqual(esperado.Login, atual.Login);
         }
     }
